e2bd62f4c239e563d6fe572f6eaf6e87b23bd3b5
Phil Trimble <PhilTrimble@gmail.com>
2013-12-20 01:45:54 -0600

    Update test_helper to include new mocha and simplecov configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 $:.unshift(File.expand_path(File.dirname(__FILE__) + '../../lib/'))
 
 $KCODE = 'u' if RUBY_VERSION =~ /^1\.8/
 
 require 'rubygems'
 require 'test/unit'
+
+require 'mocha/setup'  # The mocha docs state that this MUST come after the require for test/unit
