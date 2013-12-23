4006580cf0836ff67578be3d289160ae90bb79cb
Phil Trimble <PhilTrimble@gmail.com>
2013-12-22 22:11:52 -0600

    Add Travis CI and Coveralls status to README

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 gem 'rsync'
 gem 'mocha', :require => false  # Per mocha docs this should not be auto-loaded
 gem 'simplecov', :require => false
+gem 'coveralls', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'coveralls'
+Coveralls.wear!
+
 $:.unshift(File.expand_path(File.dirname(__FILE__) + '../../lib/'))
 
 $KCODE = 'u' if RUBY_VERSION =~ /^1\.8/
