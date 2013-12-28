d589d8af0be524aa18dbca4a1fa5c2dd6e8cb044
Phil Trimble <PhilTrimble@gmail.com>
2013-12-28 15:04:49 -0600

    Add Jeweler specs to create gem

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source "http://rubygems.org"
 gem 'rsync'
 
 group :test, :development do
-  gem 'rake'
   gem 'mocha', :require => false
   gem 'simplecov', :require => false
   gem 'coveralls', :require => false
+  gem 'jeweler'
 end
--- rakefile.rb
+++ rakefile.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path('../lib', __FILE__)
 require 'rake'
 require 'rake/testtask'
 require 'rdoc/task'
+require 'dreamhost_personal_backup'
 
 task :default => :test
 
@@ -17,4 +18,19 @@ namespace :test do
     t.test_files = FileList['test/test*.rb'].exclude('test_helper.rb')
     t.verbose = false
   end
+end
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "dreamhost-personal-backup"
+    gemspec.summary = "Provides functionality to perform personal backups (on Linux or OSX) to a Dreamhost personal backup server."
+    gemspec.description = "Provides functionality to perform personal backups (on Linux or OSX) to a Dreamhost personal backup server."
+    gemspec.email = "philtrimble@gmail.com"
+    gemspec.homepage = "https://github.com/ptrimble/dreamhost-personal-backup"
+    gemspec.version = DreamhostPersonalBackup::VERSION
+    gemspec.authors = ["Phil Trimble"]
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: gem install jeweler"
 end
