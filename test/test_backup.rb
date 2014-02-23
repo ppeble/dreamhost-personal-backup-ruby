c8a0010b2dfb6d46ca27c2f65be19ed4148af7a6
Phil Trimble <PhilTrimble@gmail.com>
2014-02-23 03:29:46 -0600

    Backup Tests: refactor test names for clarity

--- test/test_backup.rb
+++ test/test_backup.rb
@@ -4,7 +4,7 @@ require 'backup/backup'
 require 'backup/configurator'
 require 'backup/backup_logger'
 require 'backup/errors'
-require 'backup/backup_result_printer'
+require 'backup/backup_result_report'
 
 class BackupTests < Test::Unit::TestCase
 
@@ -20,7 +20,7 @@ class BackupTests < Test::Unit::TestCase
     set_expected_config_value(:host, CONFIG_PARAMETERS[:host])
   end
 
-  def test_backup_run_is_performed_without_errors_with_updated_file
+  def test_run_is_performed_without_errors_with_updated_file
     set_expected_rsync_result_as(true)
 
     backup = DreamhostPersonalBackup::Backup.new(SOURCE_DIR_PARAMETER, @configurator)
@@ -28,7 +28,7 @@ class BackupTests < Test::Unit::TestCase
     assert backup.run
   end
 
-  def test_backup_run_returns_false_if_rsync_reports_an_error
+  def test_run_returns_false_if_rsync_reports_an_error
     set_expected_rsync_result_as(false)
 
     backup = DreamhostPersonalBackup::Backup.new(SOURCE_DIR_PARAMETER, @configurator)
@@ -36,7 +36,7 @@ class BackupTests < Test::Unit::TestCase
     assert_equal false, backup.run
   end
 
-  def test_backup_initialize_raises_missing_parameter_error_if_user_is_missing
+  def test_initialize_raises_missing_parameter_error_if_user_is_missing
     set_expected_config_value(:user, nil)
 
     assert_raise(DreamhostPersonalBackup::RequiredParameterNotFoundError) {
@@ -44,7 +44,7 @@ class BackupTests < Test::Unit::TestCase
     }
   end
 
-  def test_backup_initialize_raises_missing_parameter_error_if_host_is_missing
+  def test_initialize_raises_missing_parameter_error_if_host_is_missing
     set_expected_config_value(:host, nil)
 
     assert_raise(DreamhostPersonalBackup::RequiredParameterNotFoundError) {
@@ -52,11 +52,11 @@ class BackupTests < Test::Unit::TestCase
     }
   end
 
-  def test_print_results_calls_default_printer_if_no_parameter_are_passed
-    DreamhostPersonalBackup::BackupResultPrinter.any_instance.expects(:print).returns(nil)
+  def test_generate_result_report_calls_default_reporter_if_no_parameter_are_passed
+    DreamhostPersonalBackup::BackupResultReport.any_instance.expects(:generate_for).returns(nil)
 
     backup = DreamhostPersonalBackup::Backup.new(SOURCE_DIR_PARAMETER, @configurator)
-    backup.print_results
+    backup.generate_result_report
   end
 
   private
@@ -67,11 +67,11 @@ class BackupTests < Test::Unit::TestCase
     rsync_change_array = Array.new
     rsync_change_array << rsync_change
 
-    rsync_return_code = stub(:success? => rsync_return_code, :changes => rsync_change_array, :error => "rsync example error")
+    rsync_result = stub(:success? => rsync_return_code, :changes => rsync_change_array, :error => "rsync example error")
 
     Rsync.expects(:run).with(SOURCE_DIR_PARAMETER,
                              DEST_DIR_PARAMETER,
-                             DreamhostPersonalBackup::Backup::RSYNC_COMMAND_ARGS).returns(rsync_return_code)
+                             DreamhostPersonalBackup::Backup::RSYNC_COMMAND_ARGS).returns(rsync_result)
   end
 
 
